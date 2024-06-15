24d659cd99a5bfa190e7eef792353a789aa2c894
Patrick Magee <patrick.magee@live.co.uk>
2024-06-15 22:31:58 +0100

    cleanup

---
+++ scripts/dagger-setup.ps1
@@ -0,0 +1,17 @@
+
+$DAGGER_ENGINE_POD_NAME=$(kubectl get pod --selector=name=dagger-dagger-helm-engine --namespace=dagger --output=jsonpath='{.items[0].metadata.name}')
+
+
+Set-Item -Path "Env:_EXPERIMENTAL_DAGGER_RUNNER_HOST" -Value kube-pod://${DAGGER_ENGINE_POD_NAME}?namespace=dagger
+
+$query = @"
+{
+    container {
+        from(address:"alpine") {
+            withExec(args: ["uname", "-a"]) { stdout }
+        }
+    }
+}
+"@
+
+$query | dagger query
