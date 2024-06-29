0c81e73eade898d8956e425816eeef219c576fad
Patrick Magee <patrick.magee@live.co.uk>
2024-06-30 00:01:50 +0100

    helm values

--- etc/scripts/dagger-setup.ps1
+++ etc/scripts/dagger-setup.ps1
@@ -1,6 +1,4 @@
-
 $DAGGER_ENGINE_POD_NAME=$(kubectl get pod --selector=name=dagger-dagger-helm-engine --namespace=dagger --output=jsonpath='{.items[0].metadata.name}')
-
 
 Set-Item -Path "Env:_EXPERIMENTAL_DAGGER_RUNNER_HOST" -Value kube-pod://${DAGGER_ENGINE_POD_NAME}?namespace=dagger
 
